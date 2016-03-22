0bfb490d80b57bd9af7cd8ebd0e2ead927ab0e4b
Kyler Johnson <kyler@localhost.localdomain>
2016-03-22 16:33:53 -0700

    Added Util class.  Refactored out tuple generation into the Util class

--- classes.py
+++ classes.py
@@ -1,40 +1,60 @@
 import random
 import math
 
-class Exercise:
-	pass
+class Util:
+	def generateTuple(self, length, minValue, maxValue, forceInteger = True):
+		if(forceInteger == True):
+			# We want to return integers.  Add the math.floor option.
+			result = tuple(math.floor(random.uniform(minValue, maxValue)) for x in range(length))
+		else:
+			# don't need integer values.  Remove the floor option
+			result = tuple(random.uniform(minValue, maxValue) for x in range(length))
+			
+		return result
 
+	def calculateSum(operands):
+		pass
+		
+	def calculateDifference(operands):
+		pass
+
+	def calculateProduct(operands):
+		pass
+
+	def calculateQuotient():
+		pass
+
+class Exercise:
+	def calculateResult(self):
+		pass
 
 class Addition(Exercise):
 	def __init__(self, numberOfOperands=5, minValue=0, maxValue=12):
+		self.util = Util()
 		self.numberOfOperands = numberOfOperands
 		self.minValue = minValue
 		self.maxValue = maxValue
-		self.generateQuestion()
-
-	def generateQuestion(self):
-		# We will generate a question based on the input parameters
-		# First we will make an aray of the operands
-		self.operands = tuple(math.floor(random.uniform(self.minValue, self.maxValue)) for x in range(self.numberOfOperands))
+		self.operands = self.util.generateTuple(numberOfOperands, minValue, maxValue)
 
 		# Then we will calculate the answer
-		self.answer = sum(self.operands)
+		self.calculateResult()
 
+	def calculateResult(self):
+		self.answer = sum(self.operands)
 
 class Subtraction(Exercise):
 	def __init__(self, numberOfOperands=5, minValue=0, maxValue=12):
+		self.util = Util()
 		self.numberOfOperands = numberOfOperands
 		self.minValue = minValue
 		self.maxValue = maxValue
-		self.generateQuestion()
-
-	def generateQuestion(self):
-		# We will generate a question based on the input parameters
-		# First we will make an aray of the operands
-		self.operands = tuple(math.floor(random.uniform(self.minValue, self.maxValue)) for x in range(self.numberOfOperands))
+		self.operands = self.util.generateTuple(numberOfOperands, minValue, maxValue)
 
 		# Then we will calculate the answer
-		self.answer = sum(self.operands)
+		self.calculateResult()
+
+	def calculateResult(self):
+		pass
 
 if __name__ == '__main__':
 	exercise = Addition()
